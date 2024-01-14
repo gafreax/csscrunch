2078e30d1ef32278219b49809e7570828bae7d01
Gabriele Fontana <gafreax@gmail.com>
2024-01-14 22:51:26 +0100

    fix: linting problems

--- src/lib/tokenize.d.ts
+++ src/lib/tokenize.d.ts
@@ -15,7 +15,6 @@ export interface Tokens {
   [key: string]: Token
 }
 
-
 export type GetRuleValueFunction = (params: GetRuleValueI) => string
 
 export type BuildMediaTokensFunction = (mediaTokens: BuildMediaTokensI) => Tokens
