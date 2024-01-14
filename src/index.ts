1118b13524bd47eadd21a2975d69e4d4882e4ad1
Gabriele Fontana <gafreax@gmail.com>
2024-01-14 22:56:23 +0100

    feat: add exporting from index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { tokenize } from './lib/tokenize'
 import { stringify } from './lib/stringify'
 import { compact } from './lib/compact'
-
+export { tokenize, stringify, compact }
 export const parse = (css: string): string => stringify(compact(tokenize(css)))
