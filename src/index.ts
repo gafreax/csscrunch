763a9f30cab98e975dcda0f5abb4e5217afe3f71
Gabriele Fontana <gafreax@gmail.com>
2024-01-14 22:51:07 +0100

    refactor: export parse

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,4 @@ import { tokenize } from './lib/tokenize'
 import { stringify } from './lib/stringify'
 import { compact } from './lib/compact'
 
-const cleancss = (css: string): string => stringify(compact(tokenize(css)))
-
-export default cleancss
+export const parse = (css: string): string => stringify(compact(tokenize(css)))
