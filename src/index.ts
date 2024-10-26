5a5e62332f7cc8864a3bcfc642e62dfd7da44520
Daniele Barbaro <barbaro.daniele@gmail.com>
2024-10-26 18:06:06 +0200

    Beautify parse method

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,9 @@ import { stringify } from './lib/stringify'
 import { compact } from './lib/compact'
 export { tokenize, stringify, compact }
 
-export const parse = (css: string): string => stringify(compact(tokenize(cleanCss(css))))
+export const parse = (css: string): string => {
+    const cleanedCss = cleanCss(css);
+    const tokens = tokenize(cleanedCss);
+    const compactedTokens = compact(tokens);
+    return stringify(compactedTokens);
+}
