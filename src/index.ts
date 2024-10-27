29aea941a9bb3bd3f03b942998e61423aa56c1bb
Gabriele Fontana <gafreax@gmail.com>
2024-10-27 02:39:57 +0200

    refactor: lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import {cleanCss, tokenize} from './lib/tokenize'
+import { cleanCss, tokenize } from './lib/tokenize'
 import { stringify } from './lib/stringify'
 import { compact } from './lib/compact'
 export { tokenize, stringify, compact }
 
 export const parse = (css: string): string => {
-    const cleanedCss = cleanCss(css);
-    const tokens = tokenize(cleanedCss);
-    const compactedTokens = compact(tokens);
-    return stringify(compactedTokens);
+  const cleanedCss = cleanCss(css)
+  const tokens = tokenize(cleanedCss)
+  const compactedTokens = compact(tokens)
+  return stringify(compactedTokens)
 }
