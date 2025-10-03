bd9bf1da355c0cbfc9dd2fff25e973b84dbc6027
Gabriele Fontana <gafreax@gmail.com>
2025-10-03 19:41:51 +0200

    refactor: update compile css

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,14 +5,11 @@ import fs from 'fs'
 
 import compile from '../src/index'
 
+const cssList = fs.readdirSync(path.join(__dirname, '__css__')).filter(file => file.endsWith('.css'))
 describe('compile', () => {
-  it('compiles CSS', async () => {
-    // get the list of css files in the test folder
-    const cssList = fs.readdirSync(path.join(__dirname, '__css__')).filter(file => file.endsWith('.css'))
-    for (const cssFile of cssList) {
-      const css = await readFile(path.join(__dirname, '__css__', cssFile), 'utf-8')
-      const result = compile(css)
-      expect(result).toMatchSnapshot()
-    }
+  it.each(cssList)('compiles CSS %s', async (cssFile: string) => {
+    const css = await readFile(path.join(__dirname, '__css__', cssFile), 'utf-8')
+    const result = compile(css)
+    expect(result).toMatchSnapshot()
   })
 })
