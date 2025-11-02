ebd2cb5244b7bdf0f39663382a4983a59a987895
Gabriele Fontana <gafreax@gmail.com>
2025-11-03 00:34:58 +0100

    feat: add benchmark to homepage

--- benchmark/compile.benchmark.ts
+++ benchmark/compile.benchmark.ts
@@ -3,7 +3,7 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 import compile from '../src'
 
-const css = readFileSync(join(__dirname, '__css__', 'current.css'), 'utf-8')
+const css = readFileSync(join(__dirname, '../tests/__css__', 'current.css'), 'utf-8')
 
 describe('compile', () => {
   bench('css', () => {
